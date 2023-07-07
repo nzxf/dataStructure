7946ea2560446c8e3e2531e07f9d8b003d221869
nzxf <122195155+nzxf@users.noreply.github.com>
2023-07-07 23:21:04 +0700

    Add prettyPrint

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,16 @@
 const Node = () => {
   const data = []
   let =
+
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
